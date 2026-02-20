d39f4683a61d03908a0cb869e4055af6880f6e43
Eduardo <eduardo@microsoft.com>
2026-02-20 09:55:15 +0100

    Initial Commit

---
+++ Curso-iOS-Repository/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Curso-iOS-Repository
+//
+//  Created by Equipo 2 on 20/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Curso-iOS-Repository/Curso_iOS_RepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_iOS_RepositoryApp.swift
+//  Curso-iOS-Repository
+//
+//  Created by Equipo 2 on 20/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_iOS_RepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
